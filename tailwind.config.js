6ed29072f2f9633d6c7be3338c91f9e84890f6fc
Crakleurs <eti.faviere@gmail.com>
2023-04-20 18:33:11 +0300

    add colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,15 @@
-/** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors')
+
 module.exports = {
     content: [
         "./src/**/*.{js,ts,jsx,tsx}",
     ],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                ...colors
+            }
+        },
     },
     plugins: [require("daisyui")],
 }
